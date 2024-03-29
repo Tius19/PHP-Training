abcecc188404a0045c5a33cde589eec6cd70133c
Tius19 <dhotman19@yahoo.com>
2024-03-29 14:06:14 +0400

    PHP challenge 2

---
+++ variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+    <?php
+
+    $human = "Mary";
+    $animal = "lamb";
+
+    echo $human . " had a little " . $animal;
+
+    ?>
+
+</body>
+
+</html>
