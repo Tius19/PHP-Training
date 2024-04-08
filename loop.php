d4d1a29185f7b68a2f23be7ac4042c570c5bae64
Tius19 <dhotman19@yahoo.com>
2024-04-08 16:13:56 +0400

    loop beginning

---
+++ loop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+ <head>
+
+ </head>
+
+ <body>
+    <?php
+
+      for($x = 0; $x <= 10; $x+=2) {
+        echo "$x <br>";
+      }
+
+      
+
+    ?>
+
+ </body>
+</html>
