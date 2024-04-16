1fc55f791983218c3ceac9fcf6cf54044c347e69
Tius19 <dhotman19@yahoo.com>
2024-04-16 15:57:41 +0400

    array practice

--- array.php
+++ array.php
@@ -7,13 +7,39 @@
  <body>
     <?php
 
+        function toLine() {
+            echo "<br> ------------ <br>";
+        }
+
         $colors = ["blue", "red", "green", "yellow"];
 
         foreach($colors as $x) {
             echo "<br> $x is a color <br>";
         }
 
+        
+        toLine();
+
+        array_push($colors, "orange", "purple");
+
+        sort($colors);
+
+        foreach($colors as $x) {
+            echo "<br> $x is a color <br>";
+        }
+
+        toLine();
+
         $onePiece = ["author" => "Eiichiro Oda", "MC" => "Luffy", "theme" => "pirate"];
+
+        foreach($onePiece as $x => $y) {
+            echo "<br> The $x of One Piece is $y <br>";
+        }
+
+        toLine();
+
+        $onePiece += ["swordmaster" => "Zoro", "navigator" => "Nami"];
+
 
         foreach($onePiece as $x => $y) {
             echo "<br> The $x of One Piece is $y <br>";
